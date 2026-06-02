e84d3ed0bddedd7335eb502e16cbf68e5bb53ad5
sai prerana <saiprerana.k@breewcode.co>
2026-06-02 14:44:46 +0530

    push

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+  const hobbies = ["Coding", "Reading", "Music"];
+
+  return (
+    <div>
+      <h1>Prerana</h1>
+
+      <h3>
+        Today's Date: {new Date().toLocaleDateString()}
+      </h3>
+
+      <h2>Hobbies</h2>
+
+      <ul>
+        {hobbies.map((hobby, index) => (
+          <li key={index}>{hobby}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
